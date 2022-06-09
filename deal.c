dc5ad24b3d9758252377cab074c3b2d430e1bda0
PuzzyGong <763081477@qq.com>
2022-06-09 22:41:28 +0800

    2022/6/9 22:41

--- deal.c
+++ deal.c
@@ -1,8 +1,16 @@
 #include "deal.h"
 
+//***************************************************//
+//接收和发送信息需要用到的AT指令
 unsigned char code rx1[] = "AT+CNMI=2,1\r\n";
 unsigned char code rx2[] = "AT+CMGR=1\r\n";
 unsigned char code rx3[] = "AT+CMGD=1,4\r\n";
+
+unsigned char code tx1[] = "AT+CSCS=\"GSM\"\r\n";
+unsigned char code tx2[] = "AT+CMGF=1\r\n";
+
+unsigned char code tx4[] = "AT+CMGS=\"";
+unsigned char code tx5[] = "\"\r\n";
 
 //***************************************************//
 static unsigned char strin[15] = {0};
@@ -16,10 +24,11 @@ static void Delay1ms(unsigned int x)
 		for(a = 113; a > 0; a--);
 }
 
+//用对应的pagenow显示strout
 static void show_deal(unsigned char cmd)
 {
 	signed char index = 0;
-	static unsigned char page = 1 ,pagenow = 1;
+
 	if(cmd == '+')
 		pagenow = (pagenow + 1 <= page) ? pagenow + 1 : pagenow;
 	else if(cmd == '-')
@@ -86,13 +95,17 @@ void uart_deal(unsigned char* str)
 	signed char index = 0;
 	static unsigned char flag = 0;
 	
+	static unsigned char cnt = 0;
+	static unsigned char phonenum[18] = {0};
+	static unsigned char j = 0;
+	
 	if(str[0] == '+' && str[1] == 'C' && str[2] == 'M' && str[3] == 'T' && str[4] == 'I')
-		Delay1ms(1000), send_str(rx2);
+		Delay1ms(200), send_str(rx2);
 	else if(flag == 0 && str[0] == '+' && str[1] == 'C' && str[2] == 'M' && str[3] == 'G' && str[4] == 'R')
 		flag = 1;
-	else if(flag == 1 && str[19] == 0)
-		flag = 2;
-	else if(flag == 2)
+	else if(flag == 2 && str[19] == 0) 
+		flag = 3;
+	else if(flag == 3)
 	{
 		flag = 0;
 		while(str[index] != '\r')
@@ -108,7 +121,47 @@ void uart_deal(unsigned char* str)
 			strin[index] = '\0';
 		
 		show_deal('0');
+		//SIM模块是新的那么需要初始化这两句
+		//Delay1ms(200), send_str(tx1);
+		//Delay1ms(200), send_str(tx2);
+		Delay1ms(200), send_str(tx4), send_str(phonenum), send_str(tx5);
+		if(strout[0][0] == 'E')
+			Delay1ms(200), send_str("invalid input");
+		else if(strout[0][0] == 'N')
+			Delay1ms(200), send_str("no answer found");	
+		else
+		{
+			Delay1ms(200);
+			index = page;
+			while(index--)	
+			{
+				send_str(strout[index]);
+				send_char('\n');
+			}
+		}
+		Delay1ms(200), send_char(0x1A);
+		Delay1ms(3000);
 		uart_init();
+	}
+	if(flag == 1)
+	{
+		for(index = 0; index < 19; index++)
+		{
+			if(str[index] == '\"')
+			{
+				if(cnt == 3)
+				{
+					phonenum[j] = 0;
+					j = 0;
+					cnt = 0;
+					flag = 2;
+					break;
+				}
+				cnt++;
+			}
+			else if(cnt == 3)
+				phonenum[j++] = str[index];
+		}
 	}
 }
 
